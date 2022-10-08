4d651cd6633b2cdbb8fe2b701decefa3878a2beb
adrian04981 <202011420@urp.edu.pe>
2022-10-08 17:47:54 -0500

    first commit

---
+++ Simipja Api/Controllers/PersonaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simipja_Api.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet]
+        [Route ("Listar")]
+        public dynamic ListarCliente() 
+    {
+        return new { nombre = "Jesus", edad = "33" };
+    }
+        
+//    [HttpPost]
+//    [Route {"Guardar"}]
+//        public dynamic listarGuardar()
+//{
+//}
+
+    
+
+
+    }
+}
